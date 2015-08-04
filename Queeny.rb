21ddde424e166dcb37b24dcfc0e58e006e82ac8d
Javier Soto <sotoseattle@gmail.com>
2015-08-04 08:21:14 -0700

    faster but still limited to 5x5 max

--- Queeny.rb
+++ Queeny.rb
@@ -17,14 +17,53 @@ class Queeny
 
   def fucktorize
     arr = []
-    @tb.cells.each do |x|
-      @tb.queen_cells(x.name).each do |y|
-        if x!=y
-          arr << Factor.new(vars: [y, x], vals: [0.0, 1.0, 1.0, 1.0])
-        end
-      end
+
+    # row constraints
+    (1..tb.n).each { |i| arr << fucktor(tb.row(i)) }
+
+    # col constraints
+    (1..tb.n).each { |i| arr << fucktor(tb.col(i)) }
+
+    # diag 1 constraints
+    (1..tb.n).each do |i|
+      kk = tb.oppos_grow(1, i).map{|s| tb.sqr(s).first}
+      arr << fucktor(kk) unless kk.size==1
+    end
+
+    # diag 2 constraints
+    (2..tb.n).each do |i|
+      kk = tb.oppos_grow(i, tb.n).map {|s| tb.sqr(s).first }
+      arr << fucktor(kk) unless kk.size==1
+    end
+
+    # diag 3 constraints
+    (1..tb.n).each do |i|
+      kk = tb.equal_grow(1, i).map {|s| tb.sqr(s).first }
+      arr << fucktor(kk) unless kk.size==1
+    end
+
+    # diag 4 constraints
+    (1..(tb.n-1)).each do |i|
+      kk = tb.equal_grow((i+1), 1).map {|s| tb.sqr(s).first }
+      arr << fucktor(kk) unless kk.size==1
     end
     arr
+  end
+
+  def fucktor(vars)
+    vars = vars.rotate
+    n = vars.size - 1
+    vals = nesting_loops(n).map(&:flatten)
+                           .map {|e| e.reduce(:+) > 1 ? 0.0 : 1.0}
+    Factor.new( vars: vars, vals: vals)
+  end
+
+  def nesting_loops(n)
+    if n==0
+      return [1, 0]
+    else
+      [1, 0].product(nesting_loops(n-1))
+    end
   end
 
   def highest_probability_cell
@@ -40,19 +79,18 @@ class Queeny
 
   def resolve
     ct.calibrate
+    printout
     if cell = highest_probability_cell
       f = ct.nodes.find { |n| n.vars.include?(cell) }
       ct.observation(f, cell, [1.0, 0.0])
       resolve
-    else
-      print_solution
     end
   end
 
-  def print_solution
+  def printout
       puts tb.cells.map { |x| ct.query(x, 'X') }
-             .join.gsub('1.0', 'X').gsub('0.0', '_')
-             .scan(/.{#{tb.n}}/).join("\n")
+             .map { |x| x==1.0 ? 'X' : (x==0.0 ? '_' : '?') }.join
+             .scan(/.{#{tb.n}}/).join("\n") + "\n\n"
   end
 end
 
@@ -62,8 +100,13 @@ class CliqueTree # monkey patch => reduce with Identity factor
   end
 end
 
-q = Queeny.new(4)
+q = Queeny.new(5)
 q.resolve
+
+
+# a, b, c = q.tb.cells.first(3)
+# ff = q.fucktor([a, b, c])
+# p ff.vals.flatten
 
 # Basic factor. Given x and y, cells reacheable by queen:
 #     x  y   p
